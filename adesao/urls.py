620cf0cbd869fce7875a98759ff34aeabcf7a399
Jonathan da Silva <jon-step@hotmail.com>
2019-01-11 19:53:56 -0200

    Ajustando todas as rotas

--- adesao/urls.py
+++ adesao/urls.py
@@ -25,21 +25,23 @@ urlpatterns = [
     # calendario
     url(r'^calendario/$', views.calendario, name='calendario'),
 
+    # Evento
+    url(r'^cadastrar-eventos/$', views.CadastrarEventos.as_view(), name='cadastrar_eventos'),
     url(r'^listar-eventos/$', views.ListarEventos.as_view(), name='listar_eventos'),
     url(r'^editar-evento/(?P<pk>\d+)/$', views.EditarEvento.as_view(), name='editar_evento'),
     url(r'^visualizar-evento/(?P<pk>\d+)/$', views.VisualizarEvento.as_view(), name='visualizar_evento'),
+
+    # Assistido
+    url(r'^cadastrar-assistido/$', views.CadastrarAssistido.as_view(), name='cadastrar_assistido'),
     url(r'^listar-assistidos/$', views.ListarAssistidos.as_view(), name='listar_assistidos'),
     url(r'^detalhes/(?P<pk>\d+)/$', views.DetalheAssistido.as_view(), name='detalhar_assistido'),
     url(r'^editar-assistido/(?P<pk>\d+)/$', views.EditarAssistido.as_view(), name='editar_assistido'),
 
+    # Processo
     url(r'^criar-processos/$', views.CadastrarProcesso.as_view(), name='criar_processo'),
     url(r'^lista-processos/$', views.ListaProcesso.as_view(), name='lista_processos'),
     url(r'^editar-processo/(?P<pk>\d+)/$', views.EditarProcesso.as_view(), name='editar_processo'),
     url(r'^detalhar-processo/(?P<pk>\d+)/$', views.DetalharProcesso.as_view(), name='detalhar_processo'),
-
-
-    url(r'^cadastrar-eventos/$', views.CadastrarEventos.as_view(), name='cadastrar_eventos'),
-    url(r'^cadastrar-assistido/$', views.CadastrarAssistido.as_view(), name='cadastrar_assistido'),
 
     url(r'^dados-agenda$', views.dados_agenda, name='dados_agenda'),
     ]
